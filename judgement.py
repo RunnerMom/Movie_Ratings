85d002599cc77356157aaf79634cb0bebd071ce0
Hackbright Student <gowri.grewal@gmail.com>
2013-07-19 12:19:49 -0700

    playing with fonts. not working

--- judgement.py
+++ judgement.py
@@ -13,8 +13,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    user_list = model.session.query(model.User).all()
+    user_list = model.session.query(model.User).limit(20).all()
     return render_template("user_list.html", users=user_list)
 
 if __name__== "__main__":
     app.run(debug=True)
+# ASSIGNMENT: build out some CRUDL-style views for User and Ratings objects
+# be able to create a new user
+# view a list of users
+# click on a user and view list of movies they've rated, as well as the ratings
+# be able to log in as a user
+# when logged in, be able to add or update a personal rating for a movie.
