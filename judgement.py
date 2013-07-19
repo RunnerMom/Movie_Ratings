6e70cfbe9e3d2dd8a43c020ac5c7daee9200b5c9
Hackbright Student <gowri.grewal@gmail.com>
2013-07-19 11:43:39 -0700

    home page works

--- judgement.py
+++ judgement.py
@@ -13,7 +13,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    user_list = model.session.query(model.User).all()
+    return render_template("user_list.html", users=user_list)
 
 if __name__== "__main__":
-    app.run() (debug = True)
+    app.run(debug=True)
